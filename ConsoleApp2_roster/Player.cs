7cb383afd91fd1f96efed1fe28c336daa8178c5d
BakariL <bakarilewis@gmail.com>
2018-01-29 20:27:09 -0500

    first commit

---
+++ ConsoleApp2_roster/Player.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_roster
+{
+    class Player
+    {
+        //feilds
+        private string firstName;
+        private string lastName;
+        private int madeShots;
+        private int shotAttempts;
+        private int fgPercentage;
+        private bool shooter;
+
+        //properties
+        public string fName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string Name
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public int mShots
+        {
+            get { return this.madeShots; }
+            set { this.madeShots = value; }
+        }
+
+        public int sAttempts
+        {
+            get { return this.shotAttempts; }
+            set { this.shotAttempts = value; }
+        }
+
+        public int fPercentage 
+        {
+            get { return this.fgPercentage; }
+            set { this.fgPercentage = value; }
+        }
+
+        public bool sShooter
+        {
+            get { return this.shooter; }
+            set { this.shooter = value; }
+        }
+
+        public Player()
+        {
+        }
+
+        public Player(string firstName, string lastName, int madeShots, int shotAttempts, int fgPercentage, bool shooter)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.madeShots = madeShots;
+            this.shotAttempts = shotAttempts;
+            this.fgPercentage = fgPercentage;
+            this.shooter = shooter;
+        }
+        //constructors/behaviors
+
+
+        //methods
+    }
+}
---
+++ ConsoleApp2_roster/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_roster
+{
+    class Program
+    {
+        public static object ToLower { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+
+            //Console.WriteLine("Type a number ");
+            //int input = int.Parse(Console.ReadLine());
+            //int[] numbers = new int[input];
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum = (sum + (i + 1));
+            //}
+            //Console.WriteLine(sum);
+
+
+            double[] percentage = new double[5];
+            double[] attempts = new double[5];
+            int[] makes = new int[5];
+            string choice = " ";
+            // Are you the coach? 
+            Console.WriteLine("Are you the coach?");
+            string coach = Console.ReadLine();
+            if (coach == "yes")
+            {
+                //What is the Name of your team
+                Console.WriteLine("What is the name of your team/school?");
+                string schoolName = Console.ReadLine();
+
+                //how many players on the team
+                Console.WriteLine("How many players are on the " + schoolName + "?");
+                int amountPlayers = int.Parse(Console.ReadLine());
+                string[] rosterTotal = new string[amountPlayers]; } //creates the amount of rosters spots
+                int i;
+                string playerName = ""; 
+
+                while (playerName != "quit")
+                {
+                    //Player 1, enter  name
+                    Console.WriteLine("Enter first and last name of roster spot #");
+                    //rosterTotal[] = Console.ReadLine();
+
+                    
+                    Console.WriteLine("Enter " +  " Shot attempts.");
+                    //attempts = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("How many shots"  + "make?");
+                    //makes[i] = int.Parse(Console.ReadLine());
+
+                    //percentage[i] = makes[i] / attempts[1];
+                }
+            }
+            //else if (coach == "no")
+            //{
+            //    Console.WriteLine("Welcome Player!, Pleae type your first and last name");
+            //    string playerName = Console.ReadLine();
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, Coaches and Players only, type 'ok' to try again..");
+            //} }
+            
+           
+
+       
+
+
+
+
+
+
+
+    }
+    }
+
